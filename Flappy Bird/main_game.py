d178fa83ff421df72fe3f29e20e75658af433cd0
fzhong <f.zhong1@share.epsb.ca>
2022-01-14 09:13:56 -0700

    updated

--- Flappy Bird/main_game.py
+++ Flappy Bird/main_game.py
@@ -4,7 +4,9 @@ author: Fiona Zhong
 date created: 2022-01-09
 """
 
-import pygame
+from tkinter import EventType
+from numpy import true_divide
+import pygame, os, csv
 from pygame.locals import *
 
 pygame.init()
@@ -12,18 +14,23 @@ pygame.init()
 clock = pygame.time.Clock()
 
 # game setup
-SCREEN_WIDTH = 600
-SCREEN_HEIGHT = 640
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 544
 TILE_SIZE = 32#SCREEN_HEIGHT // ROWS
 TILE_TYPES = 27
+ROWS = 20
+COL = 640
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Flappy Bird')
 
 
 #define game variables
+GRAVITY = 0.5
 ground_scroll = 0
-scroll_speed = 4
+scroll_speed = 0
+flying = False
+game_over = False
 
 #---------------load images
 # background
@@ -66,11 +73,11 @@ for i in range(2):
 
 def draw_bg():
     screen.fill(bg_colour)
-    #width = bg_img.get_width() # all widths are same
-    for x in range(4):
-        screen.blit(bg_img, (0, 0))
-        screen.blit(cloud1_img, (ground_scroll+150, SCREEN_HEIGHT - cloud1_img.get_height()+100))
-        screen.blit(cloud2_img, (ground_scroll+200, SCREEN_HEIGHT - cloud2_img.get_height()+100))
+    screen.blit(bg_img, (0, 0))
+    screen.blit(cloud1_img, (ground_scroll+SCREEN_WIDTH, 100))
+    screen.blit(cloud2_img, (ground_scroll+SCREEN_WIDTH+300, 60))
+    screen.blit(cloud2_img, (ground_scroll+SCREEN_WIDTH+400, 120))
+
 # ---------------------------------- CLASSES ---------------------------------- #
 class World():
     def __init__(self):
@@ -96,6 +103,25 @@ class World():
                         decoration_list.add(decoration)
                     elif tile  == 18: #snowman
                         pass#snowman = 
+                    elif tile >= 19 and tile <= 21: #strikes
+                        strike = Strike(img, x * TILE_SIZE, y * TILE_SIZE)
+                        strike_list.add(strike)
+                    elif tile == 22: #bat
+                        pass
+                    elif tile == 23: # sign
+                        pass
+                    elif tile == 24: #blade
+                        pass
+                    elif tile == 25: #player
+                        player = Figure('player',100, SCREEN_HEIGHT//2, 3, 1.2)
+                        player_list.add(player)
+
+        return player, strike
+    def draw(self):
+        for tile in self.obstacle_list:
+            tile[1][0] += ground_scroll
+            screen.blit(tile[0], tile[1])
+        print(ground_scroll)
 
 class Decoration(pygame.sprite.Sprite):
     def __init__(self, img, x, y):
@@ -107,6 +133,121 @@ class Decoration(pygame.sprite.Sprite):
     def update(self):
         self.rect.x += ground_scroll # scroll the world map
 
+class Figure(pygame.sprite.Sprite):
+    def __init__(self,cha_type, x, y, speed, scale):
+        super().__init__()
+        self.cha_type = cha_type
+        self.animation_list = []
+        self.action = 0
+        self.ani_index = 0
+        self.counter = 0
+
+        # Load animation image #
+        animation_type = ['fly','fall','dead']
+        for animation in animation_type:
+            aList = []
+            frame_total = len(os.listdir(f'{self.cha_type}/{animation}'))-1
+            for i in range(frame_total):
+                img = pygame.image.load(f"{self.cha_type}/{animation}/{i}.png").convert_alpha()
+                img = pygame.transform.scale(img, (int(img.get_width()*scale), int(img.get_height()*scale)))
+                aList.append(img)
+            # update all frames to the specific list within the big list
+            self.animation_list.append(aList)
+        # img variable
+        self.image = self.animation_list[self.action][self.ani_index]
+        self.rect = self.image.get_rect()
+        self.rect.center = [x, y]
+        self.width = self.image.get_width()
+        self.height = self.image.get_height()
+
+        # movement and shooting
+        self.speed = speed
+        self.velocity_y = 0
+        self.clicked = False
+        self.fall = False
+        self.shoot_cooldown = 0
+        self.shooting = False
+
+        # lives and score
+        self.alive = True
+        self.score = 0
+
+        # variables for enemies only
+        self.idling = True
+        self.vision = pygame.Rect(0, 0, 200, 20)
+
+    def update(self):
+        self.update_animation()
+
+    def movement(self):
+        # fall
+        if flying == True:
+            #gravity
+            self.fall = True
+            self.velocity_y += GRAVITY
+            if self.velocity_y > 10:
+                self.velocity_y = 10
+            if self.rect.bottom < 610:
+                self.rect.y += int(self.velocity_y)
+
+        if game_over == False:
+            #jump
+            if pygame.mouse.get_pressed()[0] == 1 and self.clicked == False:
+                self.fall = False
+                self.clicked = True
+                self.velocity_y = -5
+            if pygame.mouse.get_pressed()[0] == 0:
+                self.clicked = False
+                self.fall = True
+        # update scrolling 
+        if self.cha_type == 'player':
+            scroll_speed = self.speed
+            
+        return scroll_speed
+
+    def update_action(self, new_action):
+            # check if the new action is different to the previous one
+            if new_action != self.action:
+                self.action = new_action
+                # reset the variables that are used to update the animation
+                self.ani_index = 0
+                self.update_time = pygame.time.get_ticks()
+
+    def update_animation(self):
+        # animation
+        self.counter += 10
+        animation_cooldown = 150 # speed of frame changes, the higher # the slower
+
+        if self.counter > animation_cooldown:
+            self.counter = 0 # reset the timer
+            self.ani_index = self.ani_index + 1 # pop to the next img
+
+            # aviod animation list run out of the range (back to the start)
+            if self.ani_index >= len(self.animation_list[self.action]):
+                #if self.action == 5: # dead
+                self.ani_index = len(self.animation_list[self.action]) - 1 # stop at the last frame
+         # update image depending on current frame
+        self.image = self.animation_list[self.action][self.ani_index]
+        
+    def draw(self):
+        screen.blit(self.image,(self.rect.x, self.rect.y))
+
+class Strike(pygame.sprite.Sprite):
+    def __init__(self, img, x, y):
+        super().__init__()
+        self.image = img
+        self.rect = self.image.get_rect()
+        self.rect.midtop = (x + TILE_SIZE //2, y )
+
+    def update(self):
+        self.rect.x -= scroll_speed
+        
+    def draw(self):
+        screen.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+
 # ----------- create sprite groups --------- #
 decoration_list = pygame.sprite.Group()
 strike_list = pygame.sprite.Group()
@@ -114,7 +255,29 @@ blade_list = pygame.sprite.Group()
 coin_list = pygame.sprite.Group()
 sign_list = pygame.sprite.Group()
 enemy_list = pygame.sprite.Group()
+player_list = pygame.sprite.Group()
 
+# ------------- define classes
+player = Figure('player',100, SCREEN_HEIGHT//2, 2, 1.2)
+player_list.add(player)
+#bat = Figure('bat', )
+
+
+### World background
+#create empty tile list
+world_data = []
+for row in range(ROWS):
+    r = [-1] * COL
+    world_data.append(r)
+#load in level data and create world
+with open('level0_data.csv', newline='') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    for x, row in enumerate(reader):
+        for y, tile in enumerate(row):
+            world_data[x][y] = int(tile)
+
+world = World()
+player, strike = world.process_data(world_data)
 run = True
 while run:
 
@@ -122,15 +285,42 @@ while run:
 
     #draw background
     draw_bg()
+    world.draw()
+    # draw sprites
+    decoration_list.draw(screen)
+    decoration_list.update()
+    for strike in strike_list:
+        strike.draw()
+        strike.update()
 
-    ground_scroll -= scroll_speed
-    if abs(ground_scroll) > 35:
-        ground_scroll = 0
+    player.draw()
+    scroll_speed = player.movement()
+    player.update()
+
+    if player.rect.bottom > 610:
+        game_over = True
+        flying = False
+        player.update_action(2) # dead
+        
+    if game_over == False:
+        if player.clicked:
+            player.update_action(0) # fly
+        elif player.clicked == False and player.fall == True:
+            player.update_action(1) # fall
+        
+
+        ground_scroll = -scroll_speed
+        if abs(ground_scroll) > 5:
+            ground_scroll = 5
+       
 
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if flying == False and game_over == False:
+                flying = True
 
     pygame.display.update()
 
---
+++ twillightvania/README.md
@@ -0,0 +1,14 @@
+TwilightVania
+It is a single player platformer game that takes place in an alien forest at twilight. The goal of the main character Bluemi (blue and black skin) is to overcome all the obstacles, such as strikes, water, etc, and play around with any gears or triggers (lever and button), to open the gate to the exit.
+
+The game has 3 main levels and 1 introductory level (just straight to the exit to go into the first level). The goals of them are all to escape through the exit. 
+
+To control the player, use the key W A D to move and jump, SPACE to shoot, T to turn the lever… 
+You might meet some enemies (worm, goblin, trunk), but don’t worry, the cute mushrooms are there to help you jump higher! 
+
+Game Tips: 
+- Don’t push the box too far until it collides with the wall, because you would not be able to drag it back (it’s too heavy isn’t it?)
+- The enemies would speed up when they see you, so be careful! But sometimes they can’t control their speed and run out of the platform(suicide).  Also, they can turn the lever at some point…
+- At level 3, if you fly to the top tunnel when you touch the Blood Flask, don’t worry, that is what is supposed to happen!
+
+Please enjoy the game!
